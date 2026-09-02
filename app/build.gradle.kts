3e3befdd29af91069af8eb80daf740f6ee37ee9a
pylikv-jpg <pylik.v@gmail.com>
2026-09-02 15:19:50 +0200

    Bump TachoWatch to 0.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.pylikv.tachowatch"
         minSdk = 26
         targetSdk = 35
-        versionCode = 6
-        versionName = "0.6"
+        versionCode = 7
+        versionName = "0.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
