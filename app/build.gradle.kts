3d16d387bcd0b8db72c107cd76bae635ff669e5c
pylikv-jpg <pylik.v@gmail.com>
2026-09-02 22:36:38 +0200

    Bump weekly counter fix build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.pylikv.tachowatch"
         minSdk = 26
         targetSdk = 35
-        versionCode = 103
-        versionName = "1.0-now-history"
+        versionCode = 104
+        versionName = "1.0-week-fix"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
