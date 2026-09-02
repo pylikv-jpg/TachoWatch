9c296a4595ada0d3835482191180a5936d89447c
pylikv-jpg <pylik.v@gmail.com>
2026-09-02 19:43:16 +0200

    Bump version for TEST-11C direct card download

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.pylikv.tachowatch"
         minSdk = 26
         targetSdk = 35
-        versionCode = 21
-        versionName = "1.0-test11b"
+        versionCode = 22
+        versionName = "1.0-test11c"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
