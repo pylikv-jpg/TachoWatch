6b226550371d5cd480bf3d0b448d86d7d36e793c
pylikv-jpg <pylik.v@gmail.com>
2026-09-02 20:20:22 +0200

    Bump version for TEST-11H dynamic RX credits

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.pylikv.tachowatch"
         minSdk = 26
         targetSdk = 35
-        versionCode = 26
-        versionName = "1.0-test11g"
+        versionCode = 27
+        versionName = "1.0-test11h"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
