c4e75384eb09de4db6f3fdc820e9967e376df0e0
pylikv-jpg <pylik.v@gmail.com>
2026-09-02 16:43:49 +0200

    Bump version for TEST-9E

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.pylikv.tachowatch"
         minSdk = 26
         targetSdk = 35
-        versionCode = 14
-        versionName = "1.0-test9d"
+        versionCode = 15
+        versionName = "1.0-test9e"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
