4385f9d9c1d55e1ac378bcfd3f5f45b14450db92
pylikv-jpg <pylik.v@gmail.com>
2026-09-03 21:19:45 +0200

    Pin TachoWatch debug signing to stable keystore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
 val ciRunNumber = System.getenv("GITHUB_RUN_NUMBER")?.toIntOrNull()
 val autoVersionCode = ciRunNumber?.let { 1000 + it } ?: 112
 val autoVersionName = ciRunNumber?.let { "1.0.${1000 + it}-tachowatch" } ?: "1.0.112-tachowatch"
+val stableDebugKeystore = file("${System.getProperty("user.home")}/.android/debug.keystore")
 
 android {
     namespace = "com.pylikv.tachowatch"
@@ -20,7 +21,20 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    signingConfigs {
+        getByName("debug") {
+            storeFile = stableDebugKeystore
+            storePassword = "android"
+            keyAlias = "androiddebugkey"
+            keyPassword = "android"
+        }
+    }
+
     buildTypes {
+        getByName("debug") {
+            signingConfig = signingConfigs.getByName("debug")
+        }
+
         release {
             isMinifyEnabled = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
