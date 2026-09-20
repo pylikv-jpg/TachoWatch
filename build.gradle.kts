d2a428832edfbff83c560879839bf3f4ac1276ee
pylikv-jpg <pylik.v@gmail.com>
2026-09-20 14:29:25 +0200

    Add Kotlin static analysis to CI
    Add Android Lint, detekt 1.23.8, and ktlint 14.2.0 in non-blocking reporting mode without changing TachoWatch runtime logic.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,6 @@
 plugins {
     id("com.android.application") version "8.7.3" apply false
     id("org.jetbrains.kotlin.android") version "2.1.0" apply false
+    id("io.gitlab.arturbosch.detekt") version "1.23.8" apply false
+    id("org.jlleitschuh.gradle.ktlint") version "14.2.0" apply false
 }
